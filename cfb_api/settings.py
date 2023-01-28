aa4b414feac5f776b009479c7a85dffb804d77cd
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2023-01-28 21:16:03 +0100

    FIX: Get back to debug mode for debugging while deployed

--- cfb_api/settings.py
+++ cfb_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l2j(d08s0brvohw!wbw11!188%_#%$$!z54b)2lu72!y$-obw3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
